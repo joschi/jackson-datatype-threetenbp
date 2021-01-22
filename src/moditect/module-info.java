6300457ee3e7604c63eed3f177500ed3538d1bb0
Jochen Schalanda <jochen@schalanda.name>
2021-01-22 08:29:01 +0100

    Regenerate module-info.java with moditect-maven-plugin
    Refs #72

--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -1,12 +1,14 @@
-// Generated 28-Mar-2019 using Moditect maven plugin
+// Generated 22-Jan-2020 using Moditect maven plugin
 module com.fasterxml.jackson.datatype.threetenbp {
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires org.threeten.bp;
 
     exports com.fasterxml.jackson.datatype.threetenbp;
     exports com.fasterxml.jackson.datatype.threetenbp.deser;
     exports com.fasterxml.jackson.datatype.threetenbp.deser.key;
+    exports com.fasterxml.jackson.datatype.threetenbp.function;
     exports com.fasterxml.jackson.datatype.threetenbp.ser;
     exports com.fasterxml.jackson.datatype.threetenbp.ser.key;
 
